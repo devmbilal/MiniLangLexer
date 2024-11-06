111303f8db7d85606c0ee069feebdcbcb45a1793
devmbilal <info.bilalshakeel@gmail.com>
2024-11-06 22:18:13 +0500

    Add Token class to represent tokens with type and lexeme

---
+++ Token.java
@@ -0,0 +1,22 @@
+public class Token {
+    private TokenType type;
+    private String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + type + ", \"" + lexeme + "\")";
+    }
+}
